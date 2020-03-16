31f792fc4ee796ab3e34b5fab9984327d8bb3c7d
Samboun <Csamboun>
2020-03-16 22:04:57 +0100

    ajout html js

---
+++ Web/mapbox.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3NhbWJvdW4iLCJhIjoiY2szM2txdHNjMDAyYTNocGVsb2pyMnY4OCJ9.7hHziezMIxIKxesfL3j_Yw'
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [1.71,46.74], // starting position [lng, lat]
+    zoom: 5 // starting zoom
+    });
+;
