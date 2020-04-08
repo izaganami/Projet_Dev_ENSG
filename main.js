0df9225838297a8b52a8d6daec96602089a25612
izaganami <younesjf@outlook.fr>
2020-04-08 12:54:14 +0200

    index path

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('./Web/index.html', function (err, html) {
     if (err) {
         console.log('something bad');
         return response.end('Oops! Something bad happened.');
