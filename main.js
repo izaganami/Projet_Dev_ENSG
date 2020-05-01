d8da7189f1595758da3fb3f3b532309ce21c699c
izaganami <younesjf@outlook.fr>
2020-05-01 07:19:28 +0200

    test multiple page v3

--- main.js
+++ main.js
@@ -1,54 +1,11 @@
-var http = require('http'),
-    fs = require('fs'),
-    path = require('path'),
-    filePath = path.join(__dirname, 'Web/index.html');
+var fs = require("fs");
+var express = require("express");
+app = express.createServer();
 const PORT = process.env.PORT || 3000;
-console.log("Port:"+ PORT)
-var glob = require("glob")
-
-glob("**/*.html", function (er, files) {
-    console.log(files)
-})
-
-
-
-var app = require('http').createServer(createServer);
-var url = require('url');
-
-function createServer(req, res) {
-    var path = url.parse(req.url).pathname;
-    var fsCallback = function(error, data) {
-        if(error) throw error;
-
-        res.writeHead(200);
-        res.write(data);
-        res.end();
-    }
-
-    switch(path) {
-        case '/subpage':
-            doc = fs.readFile(__dirname + '/Web/Page_1.html', fsCallback);
-        break;
-        default:
-            doc = fs.readFile(__dirname + '/Web/index.html', fsCallback);
-        break;
-    }
-}
-
+app.use(express.static(__dirname + '/Web'));
+app.get('/', function(req, res) {
+    fs.readFile(__dirname + '/Web/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
 app.listen(PORT);
-
-/**
-fs.readFile(filePath, {encoding: 'utf-8'}, function (err, html) {
-    if (err) {
-
-        console.log('something bad');
-        console.log(err)
-
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-
-    }).listen(PORT);
-});**/
