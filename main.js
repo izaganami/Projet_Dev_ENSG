7658064a1db1b46204f02f66ed69f9cc6600da71
izaganami <younesjf@outlook.fr>
2020-05-01 07:26:41 +0200

    test multiple page v4

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-var fs = require("fs");
-var express = require("express");
-app = express.createServer();
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path');
 const PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+p=__dirname
+console.log(p)
+var server = http.createServer(app);
+
+
 app.use(express.static(__dirname + '/Web'));
 app.get('/', function(req, res) {
     fs.readFile(__dirname + '/Web/index.html', 'utf8', function(err, text){
